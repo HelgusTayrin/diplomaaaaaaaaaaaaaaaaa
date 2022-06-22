4317a39e69f99c9fc3eae61a16d061d85b1c410b
Helgus_Tayrin <44728767+HelgusTayrin@users.noreply.github.com>
2022-06-22 23:45:26 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+val koinVersion: String by project
+val kotlinVersion: String by project
+
+plugins {
+	id("org.springframework.boot") version "2.6.7"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("org.jetbrains.kotlin.plugin.jpa") version "1.5.21"
+	id("org.jetbrains.kotlin.plugin.allopen") version "1.5.21"
+	kotlin("jvm")
+	kotlin("plugin.spring")
+	application
+}
+
+allOpen {
+	annotation("javax.persistence.Entity")
+	annotation("javax.persistence.MappedSuperclass")
+	annotation("javax.persistence.Embedabble")
+}
+
+group = "com.fileService"
+version = "0.0.1"
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.6.7")
+	implementation("org.springframework.boot:spring-boot-starter-data-rest:2.6.7")
+	implementation("org.springframework.boot:spring-boot-starter-tomcat:2.6.7")
+	implementation("org.postgresql:postgresql:42.3.4")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf:2.6.7")
+
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+
+	implementation("javax.persistence:javax.persistence-api:2.2")
+	implementation("org.thymeleaf:thymeleaf:2.0.5")
+
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
